0e498fd46de5025364600b7215ee29ce8b6aaa56
sanjio3 <2433877589@qq.com>
2025-07-10 17:11:49 +0800

    Update and rename 1 to Model.php

---
+++ project_root/app/Model/Model.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Model;
+
+use PDO;
+
+class Model {
+    protected $pdo;
+
+    public function __construct() {
+        $config = include __DIR__ . '/../../config.php';
+        $dsn = "mysql:host={$config['db']['host']};dbname={$config['db']['dbname']};charset={$config['db']['charset']}";
+        $this->pdo = new PDO($dsn, $config['db']['user'], $config['db']['password']);
+    }
+}

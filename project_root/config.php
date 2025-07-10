545f4a98ea1c27dc48a1e7cf803e6cf8435397d3
sanjio3 <2433877589@qq.com>
2025-07-10 17:10:04 +0800

    Update config.php

--- project_root/config.php
+++ project_root/config.php
@@ -1,1 +1,10 @@
-
+<?php
+return [
+    'db' => [
+        'host' => '127.0.0.1',
+        'dbname' => 'webshell_scanner',
+        'user' => 'root',
+        'password' => 'your_mysql_root_password',
+        'charset' => 'utf8mb4'
+    ]
+];
